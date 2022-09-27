117ed0b97fd1e84a784652210263189440851e03
Milena-Blaser <blaser@noseryoung.com>
2022-09-27 13:50:54 +0200

    added service, controller and repo for user and role

---
+++ backend/src/main/java/todoapp/backend/role/Role.java
@@ -0,0 +1,20 @@
+package todoapp.backend.role;
+
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private int id;
+  private String name;
+}
