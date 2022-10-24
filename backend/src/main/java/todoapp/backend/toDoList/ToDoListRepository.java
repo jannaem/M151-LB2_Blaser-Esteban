ad6966c5ca2f2b57cb5d5e511a5435a1ca7cee11
Milena-Blaser <blaser@noseryoung.com>
2022-10-24 20:14:29 +0200

    added ToDoList endpoint and todolisttask

---
+++ backend/src/main/java/todoapp/backend/toDoList/ToDoListRepository.java
@@ -0,0 +1,11 @@
+package todoapp.backend.toDoList;
+
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface ToDoListRepository extends JpaRepository<ToDoList, Integer> {
+  @Query(value = "select * from to_do_list where id_user = :id", nativeQuery = true)
+  List<ToDoList> findByUserId(int id);
+}
