003de4371846bef91acf6689fa755cd0adf27451
jannaesteban <69906563+jannaesteban@users.noreply.github.com>
2022-10-25 20:15:51 +0200

    removed unnecesary files and added a theme for the project from MUI"

---
+++ frontend/theme.ts
@@ -0,0 +1,49 @@
+import { createTheme } from "@mui/material/styles";
+
+/**
+ * Defines the design of the project. It's like our color palett.
+ */
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#69CABC",
+      contrastText: "#ffffff",
+    },
+    secondary: {
+      main: "#408793",
+    },
+    success: {
+      main: "#69CABC",
+      contrastText: "#ffffff",
+    },
+    background: {
+      default: "#8eecc980",
+    },
+    action: {
+      active: "#2F4858",
+    },
+  },
+  typography: {
+    fontFamily: ["Monospace"].join(","),
+    h1: {
+      color: "#69CABC",
+    },
+    h3: {
+      color: "#69CABC",
+    },
+  },
+  components: {
+    MuiInputBase: {
+      styleOverrides: {
+        input: {
+          borderColor: "black",
+          "::before": {
+            borderBottom: "#000",
+          },
+        },
+      },
+    },
+  },
+});
+export default theme;
