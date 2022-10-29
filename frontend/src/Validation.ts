066465594a5ed8e5ef5a926a969200c7fc794776
jannaesteban <69906563+jannaesteban@users.noreply.github.com>
2022-10-29 21:50:49 +0200

    implemented registration site and validation"

--- frontend/src/Validation.ts
+++ frontend/src/Validation.ts
@@ -8,3 +8,27 @@ export const DialogFormValidation = yup.object().shape({
     .min(2, "The name has to be at least 2 characters long")
     .max(25, "test"),
 });
+
+export const RegistrationFormValidation = yup.object().shape({
+  firstName: yup
+    .string()
+    .matches(/^[aA-zZ\s]+$/, "Firstname can only contain letters")
+    .required("Username is required"),
+  lastName: yup
+    .string()
+    .matches(/^[aA-zZ\s]+$/, "Firstname can only contain letters")
+    .required("Username is required"),
+  username: yup.string().required("Username is required"),
+  password: yup
+    .string()
+    .required("Password is required")
+    .min(8, "You need at least 8 Characters")
+    .matches(/^(?=.*[0-9])/, "Must contain at least one number"),
+  email: yup.string().email().required(),
+  confirmedPassword: yup
+    .string()
+    .min(8, "You need at least 8 Characters")
+    .matches(/^(?=.*[0-9])/, "Must contain at least one number")
+    .required("Password is required")
+    .oneOf([yup.ref("password"), null], "Passwords must match"),
+});
---
+++ frontend/src/components/Pages/registration/Registration.tsx
@@ -0,0 +1,285 @@
+import { AccountCircle, Visibility, VisibilityOff } from "@material-ui/icons";
+import EmailIcon from "@mui/icons-material/Email";
+import "./Registration.css";
+import {
+  Button,
+  CircularProgress,
+  Grid,
+  IconButton,
+  InputAdornment,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React, { useContext, useState } from "react";
+import { Form, Formik, FormikValues } from "formik";
+import { FormModelRegistration } from "../../../models/Form";
+import { useNavigate } from "react-router-dom";
+import { User } from "../../../models/User";
+import ApiService from "../../../services/ApiService";
+import SnackbarContext from "../../../contexts/SnackbarContext";
+import { RegistrationFormValidation } from "../../../Validation";
+import Divider from "../../atoms/Divider/Divider";
+
+interface State {
+  password: string;
+  showPassword: boolean;
+  confirmedPassword: string;
+  showConfirmedPassword: boolean;
+}
+const Registration = () => {
+  const navigate = useNavigate();
+  const { displaySnackbarMessage } = useContext(SnackbarContext);
+  const [pwdValues, setPwdValues] = useState<State>({
+    password: "",
+    showPassword: false,
+    confirmedPassword: "",
+    showConfirmedPassword: false,
+  });
+
+  const handleClickShowPassword = () => {
+    setPwdValues({
+      ...pwdValues,
+      showPassword: !pwdValues.showPassword,
+      showConfirmedPassword: !pwdValues.showConfirmedPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
+
+  const handleSubmit = (
+    { username, password, email, lastName, firstName}: FormikValues  ) => {
+      const newUser : User = {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        username: username,
+        password: password,
+      };
+      ApiService.post("/signUp", newUser)
+      .then(() => {
+        navigate("/");
+        displaySnackbarMessage("Account created succesfully!", "success")
+      })
+      .catch((error) => {
+        displaySnackbarMessage(error.response.data, "error");
+      });
+  };
+
+  const initialValues: FormModelRegistration = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    username: "",
+    password: "",
+    confirmedPassword: "",
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={RegistrationFormValidation}
+      onSubmit={(values) => {
+        handleSubmit(values);
+      }}
+    >
+      {({
+        values,
+        errors,
+        handleChange,
+        touched,
+        isSubmitting,
+        handleBlur,
+      }) => (
+        <Form autoComplete="off">
+          <Grid container className={"registrationContainer"}>
+            <Paper
+              variant={"elevation"}
+              elevation={5}
+              className={"registrationCard"}
+            >
+              <Grid item>
+                <Typography component="h1" variant="h3" color="primary" textAlign={"center"}>
+                  <b>Sign up</b>
+                </Typography>
+              </Grid>
+              <div className={"registrationForm"}>
+                <Grid container spacing={0.8}>
+                  <Grid item className="registrationField" xs={12} md={6}>
+                    <TextField
+                      fullWidth
+                      required
+                      label="First Name"
+                      variant="outlined"
+                      name={"firstName"}
+                      value={values.firstName}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      helperText={errors.firstName && touched.firstName
+                        ? errors.firstName
+                        : null}
+                      error={errors.firstName && touched.firstName ? true : false} />
+                  </Grid>
+                  <Grid item className="registrationField" xs={12} md={6}>
+                    <TextField
+                      fullWidth
+                      required
+                      id="outlined-basic"
+                      label="Last Name"
+                      variant="outlined"
+                      name={"lastName"}
+                      value={values.lastName}
+                      helperText={errors.lastName && touched.lastName
+                        ? errors.lastName
+                        : null}
+                      error={errors.lastName && touched.lastName ? true : false}
+                      onChange={handleChange}
+                      onBlur={handleBlur} />
+                  </Grid>
+                </Grid>
+                <Grid item className="registrationField" xs={12} md={12}>
+                  <TextField
+                    fullWidth
+                    required
+                    id="outlined-basic"
+                    label="Email"
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <EmailIcon />
+                        </InputAdornment>
+                      ),
+                    }}
+                    variant="outlined"
+                    name={"email"}
+                    value={values.email}
+                    helperText={errors.email && touched.email ? errors.email : null}
+                    error={errors.email && touched.email ? true : false}
+                    onChange={handleChange}
+                    onBlur={handleBlur} />
+                </Grid>
+                <Grid item className="registrationField" xs={12} md={12}>
+                  <TextField
+                    fullWidth
+                    required
+                    label="Username"
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <AccountCircle />
+                        </InputAdornment>
+                      ),
+                    }}
+                    variant="outlined"
+                    name={"username"}
+                    value={values.username}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    helperText={errors.username && touched.username
+                      ? errors.username
+                      : null}
+                    error={errors.username && touched.username ? true : false} />
+                </Grid>
+                <Grid item className="registrationField" xs={12} md={12}>
+                  <TextField
+                    name="password"
+                    fullWidth
+                    required
+                    type={pwdValues.showPassword ? "text" : "password"}
+                    value={values.password}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    helperText={errors.password && touched.password
+                      ? errors.password
+                      : null}
+                    error={errors.password && touched.password ? true : false}
+                    label="Password"
+                    InputProps={{
+                      endAdornment: (
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          onMouseDown={handleMouseDownPassword}
+                          edge="end"
+                        >
+                          {pwdValues.showPassword ? (
+                            <Visibility />
+                          ) : (
+                            <VisibilityOff />
+                          )}
+                        </IconButton>
+                      ),
+                    }}
+                    variant="outlined" />
+                </Grid>
+                <Grid item className="registrationField" xs={12} md={12}>
+                  <TextField
+                    name="confirmedPassword"
+                    fullWidth
+                    required
+                    type={pwdValues.showConfirmedPassword ? "text" : "password"}
+                    value={values.confirmedPassword}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    helperText={errors.confirmedPassword && touched.confirmedPassword
+                      ? errors.confirmedPassword
+                      : null}
+                    error={errors.confirmedPassword && touched.confirmedPassword
+                      ? true
+                      : false}
+                    label="Confirmed Password"
+                    InputProps={{
+                      endAdornment: (
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          onMouseDown={handleMouseDownPassword}
+                          edge="end"
+                        >
+                          {pwdValues.showPassword ? (
+                            <Visibility />
+                          ) : (
+                            <VisibilityOff />
+                          )}
+                        </IconButton>
+                      ),
+                    }}
+                    variant="outlined" />
+                </Grid>
+                <Grid item className="registrationField" xs={12} md={12}>
+                  <Button
+                    variant={"contained"}
+                    type="submit"
+                    color={"primary"}
+                    fullWidth
+                    disabled={isSubmitting}
+                    startIcon={isSubmitting ? <CircularProgress size={"0.9rem"} /> : null}
+                  >
+                    create account
+                  </Button>
+                </Grid>
+                <Divider>or</Divider>
+                <Grid item className="registrationField" xs={12} md={12}>
+                  <Button
+                    variant={"contained"}
+                    color={"secondary"}
+                    disabled={isSubmitting}
+                    onClick={() => navigate("/")}
+                  >
+                    sign in
+                  </Button>
+                </Grid>
+              </div>
+            </Paper>
+          </Grid>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+export default Registration;
+
